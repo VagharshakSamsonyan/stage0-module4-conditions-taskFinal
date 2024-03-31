910b175d90b646daca7afdaf78d37b96519308f7
Vagharshak Samsonyan <vagosamsonyan@yahoo.com>
2024-03-31 19:38:24 +0400

    26 from 26 tests passed.

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -1,17 +1,18 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
-    public boolean swap(int first, int second) {
-
-        System.out.println(second);
-
-        // Perform the swap using bitwise XOR
+    public static void swap(int first, int second) {
         first = first ^ second;
         second = first ^ second;
         first = first ^ second;
-
+        System.out.println(first);
         System.out.println(second);
+    }
 
-        return first != second; // Return true if the values were different and swap was performed
+    public static void main(String[] args) {
+        int first = 5;
+        int second = 45;
+        swap(first, second);
     }
 }
+
